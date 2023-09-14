38f91723a2d5d3ec5cd2422894df5e3f7b5e379f
nileshnayak1999 <nileshnayak1999@gmail.com>
2023-09-14 23:38:06 +0530

    added script file

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,19 @@
+let js = 'amazing';
+// if(js==='amazing') alert('hiiii');
+console.log("test");
+
+
+let checkBoolean = true;
+console.log(checkBoolean);
+
+//typeof is a special operator used to show the data type in JS
+console.log(typeof checkBoolean);
+console.log(typeof true);
+console.log(typeof 99);
+
+//We always need to use single or double quotes for String
+console.log(typeof 'nilesh');
+
+//this will print nayak
+lastname='nayak'
+console.log(lastname);
